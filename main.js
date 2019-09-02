a065d47f48814690c267454b7cd4f1c813e00a72
John Adams <adamsjr8576@gmail.com>
2019-09-02 13:15:26 -0600

    Finish iteration 4

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
-var snackButton = document.getElementById('add-snack-button')
-var snackName = document.getElementById('snack-name-input')
-var snackInfo = document.getElementById('snack-desc')
-var munchieCardSection = document.getElementById('grocery-list-add')
+var snackButton = document.getElementById('add-snack-button');
+var snackName = document.getElementById('snack-name-input');
+var snackInfo = document.getElementById('snack-desc');
+var munchieCardSection = document.getElementById('grocery-list-add');
+var munchieCardName = document.getElementById('card-name');
+var munchieCardInfo = document.getElementById('card-info');
 //Iteration 1
 // When the user hits `Add Snack`, a card is created that has the munchie name and the description.
-snackButton.addEventListener('click', addSnackCard);
+snackButton.addEventListener('click', disableSnackButton);
+munchieCardSection.addEventListener('click', makeCardEditable);
+munchieCardSection.addEventListener('click', deleteSnackCard);
+snackInfo.addEventListener('change', enableSnackButton);
+snackName.addEventListener('change', enableSnackButton);
+
 
 function addSnackCard() {
 	munchieCardSection.innerHTML += `
-  <p>${snackName.value}</p>
-	<p>${snackInfo.value}</p>
-	<input type="submit" value="DELETE" class="delete-card-button">
+  <div class="grocery-list-card">
+  <p id="card-name" class="munchie-card-text">${snackName.value}</p>
+	<p id="card-info" class="munchie-card-text">${snackInfo.value}</p>
+	<button type="button" class="delete-card-button">DELETE</button>
+  </div>
 `
 }
 
 //Iteration 2
 // Edit the HTML of the dynamic snack cards to include a DELETE button. When the user clicks the DELETE button, that specific CARD is removed from the DOM
 
+function deleteSnackCard() {
+  if (event.target.classList.contains('delete-card-button')) {
+    event.target.parentNode.remove();
+  }
+}
 
 //Iteration 3
 //User can edit the name of the snack OR its description by clicking on the particular field on the CARD!
 
+function makeCardEditable() {
+  if (event.target.classList.contains('munchie-card-text')) {
+    event.target.contentEditable = true;
+  }
+}
 
 //Iteration 4
 // If there is no text in the item field OR the info field, the Add Snack button should be disabled
+
+function disableSnackButton() {
+  if(snackName.value === "" || snackInfo.value === "") {
+    snackButton.disabled = true;
+  }
+  else {
+    snackButton.disabled = false;
+    addSnackCard();
+  }
+}
+
+function enableSnackButton() {
+  if(snackName.value.length > 0 || snackInfo.value.length > 0) {
+    snackButton.disabled = false;
+  }
+}
